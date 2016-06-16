bc7f9d619774bce11d6f17219e8200a817dfa889
rozum93 <o.janiszewska93@gmail.com>
2016-06-16 23:11:17 +0200

    logowanie, które dobrze rokuje, ale jeszxcze nie działa

--- logowanie.py
+++ logowanie.py
@@ -1,9 +1,31 @@
-from sqlalchemy import create_engine
-
-sqlite_db = create_engine('sqlite:///E:\I sem\Orzechowski\projekt-z-gita\telemed')  # in RAM
+import pymysql
 
 
+# Connect to the database
+connection = pymysql.connect(host='mysql.agh.edu.pl',
+                             user='olgaj',
+                             password='DAmNw1yX',
+                             db='olgaj',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
 
-#mysql_db = create_engine('http://mysql.agh.edu.pl/phpMyAdmin/index.php', echo=True)
-#pg_db = create_engine('postgres://login:password@localhost:5432/mydatabase', echo=True)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `tbl_users` (`username`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `userid`, `password` FROM `tbl_users` WHERE `username`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
