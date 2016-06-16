c4c90d7c47091b061c7f4128dbabf683ef2f0086
rozum93 <o.janiszewska93@gmail.com>
2016-06-16 16:11:44 +0200

    nowy plik

--- logowanie.py
+++ logowanie.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine
 sqlite_db = create_engine('sqlite:///E:\I sem\Orzechowski\projekt-z-gita\telemed')  # in RAM
 
 
-#sqlite_db = create_engine('sqlite:////absolute/path/to/database.foo', echo=True) # absolute path for SQLite
-#sqlite_db = create_engine('sqlite:///db.sqlite', echo=True) # relative path
 
-#mysql_db = create_engine('mysql://localhost/database_name', echo=True)
+
+#mysql_db = create_engine('http://mysql.agh.edu.pl/phpMyAdmin/index.php', echo=True)
 #pg_db = create_engine('postgres://login:password@localhost:5432/mydatabase', echo=True)
